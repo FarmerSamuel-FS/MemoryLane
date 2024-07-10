181c76eb5ef3f53e64b24ebc3a49757ff3a6f2b8
Samuel Farmer <141048125+FarmerSamuel-FS@users.noreply.github.com>
2024-07-09 23:45:45 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,31 +1,23 @@
+mport Link from "next/link";
+ import Layout from "./layout";
 
-import Link from "next/link";
-import Layout from "../components/layout";
-
-export default function Home() {
-  return (
-    <Layout>
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">Memory Lane Blog</h1>
-        <ul className="space-y-4">
-          <li>
-            <Link href="/posts/first-post">
-              <a className="text-blue-500 hover:text-blue-700">First Post</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts/second-post">
-              <a className="text-blue-500 hover:text-blue-700">Second Post</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts/third-post">
-              <a className="text-blue-500 hover:text-blue-700">Third Post</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </Layout>
-  );
-}
-
+ export default function Home() {
+   return (
+     <Layout>
+       <div>
+         <h1>Memory Lane Blog</h1>
+         <ul>
+           <li>
+             <Link href="/posts/first-post">First Post</Link>
+           </li>
+           <li>
+             <Link href="/posts/second-post">Second Post</Link>
+           </li>
+           <li>
+             <Link href="/posts/third-post">Third Post</Link>
+           </li>
+         </ul>
+       </div>
+     </Layout>
+   );
+ }
